f57ed8363938171656e474c0e473b39413ca9c00
alaidi <abdlekhaliklaidi@gmail.com>
2026-07-29 13:53:18 +0100

    CI/CD Pipeline for Your Platform

--- client/src/interceptors/jwt.interceptor.spec.ts
+++ client/src/interceptors/jwt.interceptor.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorFn } from '@angular/common/http';
 
-import { jwtInterceptor } from './jwt.interceptor';
+import { authInterceptor } from './jwt.interceptor';
 
-describe('jwtInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => jwtInterceptor(req, next));
+describe('authInterceptor', () => {
+  const interceptor: HttpInterceptorFn = (req, next) =>
+    TestBed.runInInjectionContext(() => authInterceptor(req, next));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
